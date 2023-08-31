76b44e644a6bd21bf5e22db1cfa68b383b6187ad
Lumiinox <maximillian.michael2000@gmail.com>
2023-08-31 18:17:03 +0700

    disable eslint for some files for debug purposes

--- src/Components/GetContactList.tsx
+++ src/Components/GetContactList.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable */
 import React, { useEffect } from "react";
+/* eslint-disable */
 import { useQuery, gql } from '@apollo/client';
 import {LOAD_CONTACTS} from '../GraphQL/Queries';
 
 export function GetContactList(){
-
+/* eslint-disable */
    const {error, loading, data} = useQuery(LOAD_CONTACTS);
 
    useEffect(() => {
--- src/GraphQL/Queries.ts
+++ src/GraphQL/Queries.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import {gql} from '@apollo/client';
 
