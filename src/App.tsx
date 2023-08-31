211c809d7bbfdca3c8bd63cb1dd48ea772e7086d
Lumiinox <maximillian.michael2000@gmail.com>
2023-08-31 18:08:04 +0700

    disable eslint for some files for debug purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+/* eslint-disable */
 import React from 'react';
 import './App.css';
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from, } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
+import { GetContactList } from './Components/GetContactList';
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({message, locations, path}) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+})
+
+const link = from ([
+  errorLink,
+  new HttpLink({uri: "https://wpe-hiring.tokopedia.net/graphql"})
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+})
 
 
 function App() {
-  return <></>;
+  return (
+  <ApolloProvider client={client}>
+    <>
+      {" THIS WEBSITE IS RUNNING "}
+      <GetContactList></GetContactList>
+    </>
+  </ApolloProvider>);
 }
 
 export default App;
